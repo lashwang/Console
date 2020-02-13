11b8edb89a4c5f176c21672054ca3fc41c5fae83
lashwang <lashwang@outlook.com>
2020-02-13 10:34:09 +0800

    code update.

--- app/src/main/java/com/gs/console/main/Main.java
+++ app/src/main/java/com/gs/console/main/Main.java
@@ -8,7 +8,10 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args){
         TrustedCertificateStore mStore = new TrustedCertificateStore();
-        Set<String> user = mStore.userAliases();
-        System.out.println("User cert number:" + user.size());
+        Set<String> userList = mStore.userAliases();
+        System.out.println("User cert number:" + userList.size());
+        for(String alias:userList){
+            System.out.println("User alias:" + alias);
+        }
     }
 }
