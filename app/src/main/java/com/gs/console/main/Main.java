3e808bd7a03662af70e5c0c5386e0de2e942eea2
lashwang <lashwang@outlook.com>
2020-02-17 11:45:29 +0800

    code update.

--- app/src/main/java/com/gs/console/main/Main.java
+++ app/src/main/java/com/gs/console/main/Main.java
@@ -6,10 +6,21 @@ import java.util.List;
 import java.util.ArrayList;
 
 
+
+
+
 public class Main {
+
+    private static final String PreinstalledCAAlias = "user:1cc67baa.0";
+
+
+
     public static void main(String[] args){
-        if("install_ca".equals(args[0])){
-            install_ca();
+        if(args.length >= 1){
+            if("install_ca".equals(args[0])){
+                delete_all_user_ca();
+                install_ca();
+            }
         }else {
             test_gs_security_api();
         }
@@ -20,6 +31,16 @@ public class Main {
         GsSecurityManager.instance().installCaCert("system/etc/console_assets/cacrt.crt");
     }
 
+    public static void delete_all_user_ca(){
+        List<String> all_CAs = GsSecurityManager.instance().getAllCaAliases();
+        for(String alias:all_CAs){
+            if(alias.contains("user")){
+                GsSecurityManager.instance().decodeCaAlias(alias);
+            }
+        }
+    }
+
+
     public static void test_gs_security_api(){
         List<String> system_ca_list =  GsSecurityManager.instance().getSystemCaAliases();
 
@@ -28,13 +49,13 @@ public class Main {
             System.out.println(alias);
         }
 
-        String path = GsSecurityManager.instance().fileForCaAlias("user:a6a23a50.0");
+        String path = GsSecurityManager.instance().fileForCaAlias(PreinstalledCAAlias);
         System.out.println(path);
 
 
         List<String> ca_list = new ArrayList<>();
         ca_list.add("system:5046c355.0");
-        ca_list.add("user:a6a23a50.0");
+        ca_list.add(PreinstalledCAAlias);
         ca_list.add("system:10531352.0");
 
         GsSecurityManager.instance().combineCaAliasToFile(ca_list,"/data/misc/wifi/ca.pem");
